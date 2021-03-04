ce65b18e6fd9514381d58b75a61c7dc5f2733165
Harman Bhutani <byteharman0101@gmail.com>
2021-03-04 01:06:19 -0500

    alignment of project description

--- client/src/view/ProjectDetailed/ProjectDetailedInfo.js
+++ client/src/view/ProjectDetailed/ProjectDetailedInfo.js
@@ -1,68 +1,67 @@
-import React from "react";
+import React, { Component, PropTypes } from 'react';
 import { Segment, Grid, Icon } from "semantic-ui-react";
 import "./projectdetail.css"
+import "./ProjectDeatiledPage"
 
-/**
- * @author @binjiasata
- * @description project details included date, description, skills
- *
- */
+class ProjectDetailedInfo extends Component {
 
+  constructor(props) {
+    super(props);
+  }
+  render()
+  {
+    if(this.props.project.description)
+    {
+    var nlprojectdesc = this.props.project.description;  
+    var projectdesc = nlprojectdesc.toLowerCase();
+    var lenofdesc = projectdesc.length;
+    var indexofskills = projectdesc.indexOf("skills:");
+    var indexofprojecttype = projectdesc.indexOf("project type:");
+    var indexofdomain = projectdesc.indexOf("domains:");
+    var prjct;
+    if(indexofprojecttype!==-1&&indexofdomain!==-1&&indexofskills!==-1)
+    {
+    prjct = nlprojectdesc.slice(0,indexofprojecttype);
+    var prjct_type = nlprojectdesc.slice(indexofprojecttype,indexofdomain);
+    var prjct_domain = nlprojectdesc.slice(indexofdomain,indexofskills);
+    var prjct_skill = nlprojectdesc.slice(indexofskills,lenofdesc);
+    }
+    else
+    {
+    prjct= nlprojectdesc;
+    }
+    document.getElementById("prjct_desc").innerHTML="<p>"+prjct+"</p>";
+    if(indexofprojecttype===-1)
+    {
+      document.getElementById("prjct_type").style.display="none";
+    }
+    else
+    {
+      document.getElementById("prjct_type").style.display="block";
+      document.getElementById("prjct_type").innerHTML="<hr><p>"+prjct_type+"</p>";
+    }
+    if(indexofdomain===-1)
+    {
+      document.getElementById("prjct_domain").style.display="none";
+    }
+    else
+    {
+      document.getElementById("prjct_domain").style.display="block";
+      document.getElementById("prjct_domain").innerHTML="<hr><p>"+prjct_domain+"</p>";
+    }
+    if(indexofskills===-1)
+    {
+      document.getElementById("prjct_skill").style.display="none";
+    }
+    else
+    {
+      document.getElementById("prjct_skill").style.display="block";
+      document.getElementById("prjct_skill").innerHTML="<hr><p>"+prjct_skill+"</p>";
+    }
 
-const ProjectDetailedInfo = ({ project }) => {
- 
+  }
   
-
-  window.onload = (event) => {
-  var nlprojectdesc = project.description;  
-  var projectdesc = nlprojectdesc.toLowerCase();
-  var lenofdesc = projectdesc.length;
-  var indexofskills = projectdesc.indexOf("skills:");
-  var indexofprojecttype = projectdesc.indexOf("project type:");
-  var indexofdomain = projectdesc.indexOf("domains:");
-  var prjct;
-  if(indexofprojecttype!==-1&&indexofdomain!==-1&&indexofskills!==-1)
-  {
-  prjct = nlprojectdesc.slice(0,indexofprojecttype);
-  var prjct_type = nlprojectdesc.slice(indexofprojecttype,indexofdomain);
-  var prjct_domain = nlprojectdesc.slice(indexofdomain,indexofskills);
-  var prjct_skill = nlprojectdesc.slice(indexofskills,lenofdesc);
-  }
-  else
-  {
-  prjct= nlprojectdesc;
-  }
-  document.getElementById("prjct_desc").innerHTML="<p>"+prjct+"</p>";
-  if(indexofprojecttype===-1)
-  {
-    document.getElementById("prjct_type").style.display="none";
-  }
-  else
-  {
-    document.getElementById("prjct_type").style.display="block";
-    document.getElementById("prjct_type").innerHTML="<hr><p>"+prjct_type+"</p>";
-  }
-  if(indexofdomain===-1)
-  {
-    document.getElementById("prjct_domain").style.display="none";
-  }
-  else
-  {
-    document.getElementById("prjct_domain").style.display="block";
-    document.getElementById("prjct_domain").innerHTML="<hr><p>"+prjct_domain+"</p>";
-  }
-  if(indexofskills===-1)
-  {
-    document.getElementById("prjct_skill").style.display="none";
-  }
-  else
-  {
-    document.getElementById("prjct_skill").style.display="block";
-    document.getElementById("prjct_skill").innerHTML="<hr><p>"+prjct_skill+"</p>";
-  }
-};
-
-  return (
+return(
     <Segment.Group>
       <Segment attached="top">
         <Grid verticalAlign="middle">
@@ -71,8 +70,8 @@ const ProjectDetailedInfo = ({ project }) => {
           </Grid.Column>
           <Grid.Column width={15}>
             <span>
-              {project.postedOn}
-              {project.postedOn ? "  To  " + project.validUntil : ""}
+              {this.props.project.postedOn}
+              {this.props.project.postedOn ? "  To  " + this.props.project.validUntil : ""}
             </span>
           </Grid.Column>
         </Grid>
@@ -96,12 +95,17 @@ const ProjectDetailedInfo = ({ project }) => {
             <Icon size="large" color="teal" name="code branch" />
           </Grid.Column>
           <Grid.Column width={15}>
-            <p>{project.skills}</p>
+            <p>{this.props.project.skills}</p>
           </Grid.Column>
         </Grid>
       </Segment>
     </Segment.Group>
-  );
-};
+)
+  };
+}
+
+
+
 
 export default ProjectDetailedInfo;
+
