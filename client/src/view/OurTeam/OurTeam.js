7c8b9a031fffc10557f3929a5eadc5763d4ffc23
Akansha Agnihotri <shubhiakansha96@gmail.com>
2021-05-17 10:05:29 +0530

    Team Page Info Update

--- client/src/view/OurTeam/OurTeam.js
+++ client/src/view/OurTeam/OurTeam.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from "react";
 import { Image, Grid, Segment } from "semantic-ui-react";
 import './OurTeam.css';
+import {TeamMembers} from './TeamMembers'
 
 const Home = (props) => {
   const InitialArray = new Array([10]);
@@ -31,179 +32,18 @@ const Home = (props) => {
       <Segment placeholder>
         <Grid columns={4} stackable textAlign="center">
           <Grid.Row>
-            <Grid.Column>
-              <Image id="user_image" centered src="/assets/karam.jpg" />
-              <h3>Karamvir Singh</h3>
-              <h3>President</h3>
-              <p>ksing108@uottawa.ca</p>
-              {/* <Accordion>
-                <Accordion.Title
-                  active={activeIndex === 0}
-                  index={0}
-                  onClick={handleClick}
-                >
-                  <Icon name="dropdown" />
-                  {bios[0]}
-                </Accordion.Title>
-                <Accordion.Content active={activeIndex === 0}>
-                  <p>
-                    Introduction 1
-                  </p>
-                </Accordion.Content>
-              </Accordion> */}
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image"  src="/assets/yillin.jpg" />
-              <h3>Yilin Yang</h3>
-              <h3>Vice-President</h3>
-              <p>yyang308@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image"  src="/assets/fahim.png" />
-              <h3>Mohammed Fahim</h3>
-              <h3>Event Manager</h3>
-              <p>mfahi009@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image" src="/assets/chinmaye.jpg" />
-              <h3>Chinmayee Mahagaonkar</h3>
-              <h3>Event Manager</h3>
-              <p>cmaha026@uottawa.ca</p>
-            </Grid.Column>
+            {TeamMembers.map((teamMember,index)=><Grid.Column>
+              <Image id={index} centered src={teamMember.imagePath} />
+              <h3>{teamMember.name}</h3>
+              <h3>{teamMember.position}</h3>
+              <p>{teamMember.mailId}</p>
+            </Grid.Column>)}
           </Grid.Row>
-
-          <Grid.Row>
-          <Grid.Column>
-              <Image centered id="user_image" src="/assets/Flora.jpg" />
-              <h3>Yi (Flora) Liu</h3>
-              <h3>Alumni Relation Coordinator</h3>
-              <p>yliu538@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image" src="/assets/astha.jpeg" />
-              <h3>Astha Tiwari</h3>
-              <h3>Alumni Relation Coordinator</h3>
-              <p>atiwa061@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image" src="/assets/ajay.jpeg" />
-              <h3>Ajay Modagi</h3>
-              <h3>Alumni Relation Coordinator</h3>
-              <p>amoda040@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image" src="/assets/shubhneet.jpeg" />
-              <h3>Shubhneet Baath</h3>
-              <h3>Publications Director</h3>
-              <p>sbaat080@uottawa.ca</p>
-            </Grid.Column>          
-          </Grid.Row>
-          <Grid.Row>
-          <Grid.Column>
-              <Image centered id="user_image" src="/assets/roopleen.jpg" />
-              <h3>Roopleen Kaur</h3>
-              <h3>Project Manager</h3>
-              <p>rkaur050@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image" src="/assets/mansher.jpg" />
-              <h3>Mansher Singh</h3>
-              <h3>Program Coordinator Lead</h3>
-              <p>msidh098@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image" src="/assets/Nagasudheer.jpeg" />
-              <h3>Nagasudheer Adusumilli</h3>
-              <h3>(uOzone) Navigator Manager</h3>
-              <p>ahasa085@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image" src="/assets/abdullah.jpg" />
-              <h3>Abdullah Hassan</h3>
-              <h3>Finance Manager/Youtube Coordinator</h3>
-              <p>ahasa085@uottawa.ca</p>
-            </Grid.Column>
-          </Grid.Row>
-
-          <Grid.Row>
-          <Grid.Column>
-              <Image centered id="user_image" src="/assets/tayo.jpg" />
-              <h3>Olutayo Oladosu</h3>
-              <h3>Social Media Coordinator</h3>
-              <p>oolad085@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image" src="/assets/Akansha.png" />
-              <h3>Akansha Agnihotri</h3>
-              <h3>Social Media Coordinator</h3>
-              <p>aagni073@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image" src="/assets/suhani.jpg" />
-              <h3>Suhani Jatheliya</h3>
-              <h3>Social Media Coordinator</h3>
-              <p>sjath077@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image" src="/assets/Shivani.jpg" />
-              <h3>Shivani Vermaa</h3>
-              <h3>Social Media Coordinator</h3>
-              <p>sverm020@uottawa.ca</p>
-            </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-          <Grid.Column>
-              <Image centered id="user_image" src="/assets/sahana.jpg" />
-              <h3>Sahana Sundar Raman</h3>
-              <h3>YouTube Coordinator</h3>
-              <p>ssaha062@uottawa.ca</p>
-              </Grid.Column>
-              <Grid.Column>
-            <Image centered id="user_image" src="/assets/siddi.jpg" />
-              <h3>Siddhi Deshmukh</h3>
-              <h3>YouTube Coordinator</h3>
-              <p>sdesh031@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-              <Image centered id="user_image" src="/assets/Navpreet.jpg" />
-              <h3>Navpreet Kaur</h3>
-              <h3>Web Master</h3>
-              <p>nkaur051@uottawa.ca</p>
-              </Grid.Column>
-              <Grid.Column>
-            <Image centered id="user_image" src="/assets/sunakshi.jpg" />
-              <h3>Sunakshi Sharma</h3>
-              <h3>Quality Assurance</h3>
-              <p>sshar222@uottawa.ca</p>
-            </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-            <Grid.Column>
-            <Image centered id="user_image" src="/assets/harman.jpg" />
-              <h3>Harman Preet Singh</h3>
-              <h3>Web Developer</h3>
-              <p>hbhut065@uottawa.ca</p>
-            </Grid.Column>
-            <Grid.Column>
-            <Image centered id="user_image" src="/assets/sourabh.jpg" />
-              <h3>Sourabh Agarwal</h3>
-              <h3>Web Developer</h3>
-              <p>sagar044@uottawa.ca</p>
-            </Grid.Column>
-          <Grid.Column>
-            <Image centered id="user_image" src="/assets/krishna.jpg" />
-              <h3>Achyuth Krishna Chepuri</h3>
-              <h3>Web Developer</h3>
-              <p>achep065@uottawa.ca</p>
-            </Grid.Column>                   
-            </Grid.Row>
-
             <Grid.Row>
             <a class="item" href="#/program-coordinators" color="blue">
-             <h3><font color="#4183c4">PDC Student Representative for Your Program</font></h3> 
+             <h3><font color="#4183c4">PDC Student Representative for Your Program</font></h3>
             </a>
             </Grid.Row>
-         
         </Grid>
       </Segment>
     </Fragment>
---
+++ client/src/view/OurTeam/TeamMembers.js
@@ -0,0 +1,88 @@
+export const TeamMembers=[
+    {name: "Mohammed Fahim",
+        position:"President",
+        mailId:"mfahi009@uotttawa.ca",
+        imagePath:"/assets/fahim.png"
+    },
+    {name: "Roopleen Kaur",
+        position:"Vice President",
+        mailId:"rkaur050@uottawa.ca",
+        imagePath:"/assets/roopleen.jpeg"
+    },
+    {name: "Chinmayee Mahagaonkar",
+        position:"Vice President - Industry Relations",
+        mailId:"cmaha026@uottawa.ca",
+        imagePath:"/assets/chinmaye.jpg"
+    },
+    {name: "Abdullah-Al-Mehedi Hasan",
+        position:"Finance/Youtube",
+        mailId:"ahasa085@uottawa.ca",
+        imagePath:"/assets/abdullah.jpg"
+    },
+    {name: "Shivani Verma",
+        position:"Volunteering Management Executive",
+        mailId:"sverm020@uottawa.ca",
+        imagePath:"/assets/shivani.jpg"
+    },
+    {name: "Muhammad Atif",
+        position:"Event Manager",
+        mailId:"matif062@uottawa.ca",
+        imagePath:"/assets/atif.jpeg"
+    },
+    {name: "Samhita Kuili",
+        position:"Event Manager",
+        mailId:"skuil016@uottawa.ca",
+        imagePath:"/assets/samhita.jpeg"
+    },
+    {name: "Olutayo Oladosu",
+        position:"Social Media Manager",
+        mailId:"oolad085@uottawa.ca",
+        imagePath:"/assets/tayo.jpeg"
+    },
+    {name: "Akansha Agnihotri",
+        position:"Web Master & Social Media Manager",
+        mailId:"aagni073@uottawa.ca",
+        imagePath:"/assets/akansha.jpg"
+    },
+    {name: "Shubhneet Baath",
+        position:"Publications Director",
+        mailId:"sbaat080@uottawa.ca",
+        imagePath:"/assets/shubhneet.jpeg"
+    },
+    {name: "Abhiraj Singh Sarna",
+        position:"Website Team Lead",
+        mailId:"asarn058@uottawa.ca",
+        imagePath:"/assets/abhiraj.jpeg"
+    },
+    {name: "Mansher Sidhu",
+        position:"Lead Program Coordinator",
+        mailId:"msidh098@uottawa.ca",
+        imagePath:"/assets/mansher.jpg"
+    },
+    {name: "Hind Tlassellal",
+        position:"Bilingual Initiative Manager",
+        mailId:"htlas006@uottawa.ca",
+        imagePath:"/assets/hind.jpg"
+    },
+    {name: "Karamvir Singh",
+        position:"Alumni Coordinator",
+        mailId:"ksing108@uottawa.ca",
+        imagePath:"/assets/karam.jpg"
+    },
+    {name: "Siddhi Deshmukh",
+        position:"Youtube Coordinator",
+        mailId:"sdesh031@uottawa.ca",
+        imagePath:"/assets/siddhi.jpg"
+    },
+    {name: "Sahana Sundar Raman",
+        position:"Youtube Coordinator",
+        mailId:"ssaha062@uottawa.ca",
+        imagePath:"/assets/sahana.jpg"
+    },
+    {name: "Carvi Jain",
+        position:"Content & Graphics Team Volunteer",
+        mailId:"cjain034@uottawa.ca",
+        imagePath:"/assets/carvi.png"
+    },
+
+]
