114c3e2bb38af7fb783d8be2b01cf799283f4212
Akansha Agnihotri <shubhiakansha96@gmail.com>
2021-07-20 21:15:43 -0400

    Update Alumni Page

--- client/src/view/Alumni/AlumniMembers.js
+++ client/src/view/Alumni/AlumniMembers.js
@@ -1,10 +1,5 @@
 export const AlumniMembers=[
-    {name: "Akashdeep Singh",
-        position:"Graduated from University of Ottawa",
-        mailId:"asing255@uottawa.ca",
-        imagePath:"/assets/akashdeep.jpg",
-        linkedinId:"www.linkedin.com/in/akashdeep-singh1997",
-    },
+
     {name: "Ajay Modagi",
         position:"Dissemination Officer at Statistics Canada",
         mailId:"amoda040@uottawa.ca",
@@ -16,5 +11,11 @@ export const AlumniMembers=[
         mailId:"nkaur051@uottawa.ca",
         imagePath:"/assets/navpreet.jpg",
         linkedinId: "https://www.linkedin.com/in/navpreet-kaur-uottawa/"
+    },
+    {name: "Akashdeep Singh",
+        position:"Graduated from University of Ottawa",
+        mailId:"asing255@uottawa.ca",
+        imagePath:"/assets/akashdeep.jpg",
+        linkedinId:"www.linkedin.com/in/akashdeep-singh1997",
     },
 ]
