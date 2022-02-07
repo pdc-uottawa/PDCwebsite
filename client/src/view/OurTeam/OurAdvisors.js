7252cf45f1e88497cb8ecff437ea248fd132547c
GSingh777 <gursimratsingh1998@gmail.com>
2022-02-06 23:14:18 -0500

    Updating Our Teams page

---
+++ client/src/view/OurTeam/OurAdvisors.js
@@ -0,0 +1,11 @@
+export const OurAdvisors=[
+    {   name: "Prof Liam Peyton",
+        imagePath:"/assets/liam_peyton3.jpg",
+        linkedinId:"https://www.linkedin.com/in/liam-peyton-8346812",
+    },
+    {   name: "Dr. Sawsan Abdul- Majid",
+        imagePath:"/assets/sawson.png",
+        linkedinId: "https://www.linkedin.com/in/drsawsanmajid"
+    },
+
+]
