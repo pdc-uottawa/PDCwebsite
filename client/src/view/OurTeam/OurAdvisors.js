83116434929d617c70fb9b765543afb97b97070b
GSingh777 <gursimratsingh1998@gmail.com>
2022-02-07 20:46:28 -0500

    Changing information for Faculty Advisors in Our Teams Page

--- client/src/view/OurTeam/OurAdvisors.js
+++ client/src/view/OurTeam/OurAdvisors.js
@@ -1,10 +1,12 @@
 export const OurAdvisors=[
-    {   name: "Prof Liam Peyton",
+    {   name: "Dr. Liam Peyton",
         imagePath:"/assets/liam_peyton3.jpg",
+        position:"Vice-Dean Graduate Studies for the Faculty of Engineering",
         linkedinId:"https://www.linkedin.com/in/liam-peyton-8346812",
     },
     {   name: "Dr. Sawsan Abdul- Majid",
         imagePath:"/assets/sawson.png",
+        position:"President, ANCWT",
         linkedinId: "https://www.linkedin.com/in/drsawsanmajid"
     },
 
