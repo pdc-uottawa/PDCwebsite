ddc34380520df705e24526e4171a2ace49d76f44
Amneet-Singh <asing288@uottawa.ca>
2022-06-28 20:50:51 -0400

    Added ids to homepage buttons

--- client/src/view/Home/Carousel.js
+++ client/src/view/Home/Carousel.js
@@ -1,10 +1,7 @@
-import React, { useEffect, useState } from "react";
-import Carousel from "react-alice-carousel";
+import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
-import { config } from "../../common/config/config";
-import Axios from "axios";
-import { Spinner } from "react-activity";
+
 import { Icon, Button } from "semantic-ui-react";
 
 function ImageCarousel(props) {
@@ -13,39 +10,51 @@ function ImageCarousel(props) {
   function handleDownScroll() {
     window.scrollTo({
       top: window.innerHeight - 70,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   }
 
   return (
     <>
-      <AliceCarousel disableDotsControls autoPlay controlsStrategy="alternate" autoPlayInterval={"10000"} infinite >
-        {
-          carouselData.map((car, index) => {
-            return (
-              <>
-                <div className="center">
-                  <img className="car-img" src={car.image} />
+      <AliceCarousel
+        disableDotsControls
+        autoPlay
+        controlsStrategy="alternate"
+        autoPlayInterval={"10000"}
+        infinite
+      >
+        {carouselData.map((car, index) => {
+          return (
+            <>
+              <div className="center">
+                <img className="car-img" src={car.image} />
+              </div>
+              <div
+                className="ScrollDownButton"
+                id="scrollDownButton-homepageCarousel"
+                onClick={handleDownScroll}
+              >
+                <Icon name="angle down" color="white" size="huge" />
+              </div>
+              {index === 0 ? (
+                <div
+                  className="JTTButton"
+                  id="joinTeam-carousel"
+                  onClick={() =>
+                    window.open(
+                      "https://forms.office.com/Pages/ResponsePage.aspx?id=sdof1BV-_Uy1-nIA5U3ra5WauXDgBfFLkMzBuH0SCR9UOElJOExDSjRON1c2RElYVTY3STY0V0NNVC4u",
+                      "_blank"
+                    )
+                  }
+                >
+                  <Button>
+                    <p>Join The Team</p>
+                  </Button>
                 </div>
-                <div className="ScrollDownButton" onClick={handleDownScroll}>
-                  <Icon name="angle down" color="white" size="huge" />
-                </div>
-                {
-                  index === 0 ? 
-                  <div className="JTTButton" onClick={() =>
-                      window.open(
-                        "https://forms.office.com/Pages/ResponsePage.aspx?id=sdof1BV-_Uy1-nIA5U3ra5WauXDgBfFLkMzBuH0SCR9UOElJOExDSjRON1c2RElYVTY3STY0V0NNVC4u",
-                        "_blank"
-                      )}>
-                    <Button><p>Join The Team</p></Button>
-                  </div>
-                  :
-                  null
-                }
-              </>
-            )
-          })
-        }
+              ) : null}
+            </>
+          );
+        })}
       </AliceCarousel>
     </>
   );
