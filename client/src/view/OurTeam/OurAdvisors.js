9fba027a4dc96a8947aa3df9740eb0bc2a3c74b4
Khushpreet Kaur <khushilochb@gmail.com>
2022-08-14 22:11:54 -0400

    Added New Faculty Advisor

--- client/src/view/OurTeam/OurAdvisors.js
+++ client/src/view/OurTeam/OurAdvisors.js
@@ -1,13 +1,21 @@
-export const OurAdvisors=[
-    {   name: "Dr. Liam Peyton",
-        imagePath:"/assets/liam_peyton3.jpg",
-        position:"Vice-Dean Graduate Studies for the Faculty of Engineering",
-        linkedinId:"https://www.linkedin.com/in/liam-peyton-8346812",
+export const OurAdvisors = [
+    {
+        name: "Dr. Liam Peyton",
+        imagePath: "/assets/liam_peyton3.jpg",
+        position: "Vice-Dean Graduate Studies for the Faculty of Engineering",
+        linkedinId: "https://www.linkedin.com/in/liam-peyton-8346812",
     },
-    {   name: "Dr. Sawsan Abdul- Majid",
-        imagePath:"/assets/sawson.png",
-        position:"President, ANCWT",
+    {
+        name: "Dr. Sawsan Abdul- Majid",
+        imagePath: "/assets/sawson.png",
+        position: "President, ANCWT",
         linkedinId: "https://www.linkedin.com/in/drsawsanmajid"
     },
+    {
+        name: "Dr. Hanan Anis",
+        imagePath: "/assets/hanan.png",
+        position: "Director of the School of Engineering Design and Teaching Innovation",
+        linkedinId: "https://www.linkedin.com/in/hanan-anis-5b946b11"
+    }
 
 ]
