2ca506ff33d242cde5b229aa6aa503b4393a4b46
Amneet Singh <asing288@uottawa.ca>
2022-09-15 20:09:25 -0400

    Aligned project details page

--- client/src/view/ProjectDetailed/ProjectDetailedPageV2.js
+++ client/src/view/ProjectDetailed/ProjectDetailedPageV2.js
@@ -19,106 +19,103 @@ import { UserContext } from "../../common/context/UserProvider";
 const path = config();
 
 const ProjectDeatiledPageNeww = (props) => {
-    const { userInfo, setUserInfo } = useContext(UserContext);
-    const [projectDetails, setProjectDetails] = useState({});
-    const [appliedStudentsList, setAppliedStudentsList] = useState([]);
-    const [width, setWidth] = useState(null);
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  const [projectDetails, setProjectDetails] = useState({});
+  const [appliedStudentsList, setAppliedStudentsList] = useState([]);
+  const [width, setWidth] = useState(null);
 
-    const { id } = props.match.params;
+  const { id } = props.match.params;
 
-    useEffect(() => {
-        setWidth(window.innerWidth)
-    }, [])
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
 
-    // get project details according to project id
-    useEffect(() => {
-        Axios.get(path + "project/" + id)
-            .then((res) => {
-                return res.data;
-            })
-            .then((data) => {
-                //return projectDetails;
-                setProjectDetails(data);
-            });
+  // get project details according to project id
+  useEffect(() => {
+    Axios.get(path + "project/" + id)
+      .then((res) => {
+        return res.data;
+      })
+      .then((data) => {
+        //return projectDetails;
+        setProjectDetails(data);
+      });
 
-        Axios.get(path + "student/apply/" + id)
-            .then((res) => {
-                return res.data;
-            })
-            .then((data) => {
-                setAppliedStudentsList(data);
-            });
-    }, [setProjectDetails, id]);
+    Axios.get(path + "student/apply/" + id)
+      .then((res) => {
+        return res.data;
+      })
+      .then((data) => {
+        setAppliedStudentsList(data);
+      });
+  }, [setProjectDetails, id]);
 
-    function chckadmin(ad) {
-        return ad.email === userInfo.user.email
-    }
+  function chckadmin(ad) {
+    return ad.email === userInfo.user.email;
+  }
 
-    return (
-        <Grid>
-            <Helmet>
+  return (
+    <Grid>
+      <Helmet>
         <title>Project Detail | Professional Development Club</title>
       </Helmet>
-            {
-                width < 768 ?
-                <>
-                    <ProjectName
-                        id={id}
-                        path={path}
-                        project={projectDetails}
-                        userInfo={userInfo}
-                        appliedStudentsList={appliedStudentsList}
-                    />
-                    <ProjectInfo
-                        project={projectDetails} 
-                    />
-                    <ProjectDates project={projectDetails} />
-                    <ProjectHost project={projectDetails} />
-                    <>
-                        {userInfo.user &&
-                            (userInfo.user.company || userInfo.user.admin) &&
-                            projectDetails.user &&
-                            projectDetails.user.findIndex(chckadmin) !== -1 ? (
-                            <ProjectDetailedAppliedStudents
-                                appliedStudentsList={appliedStudentsList}
-                            />
-                        ) : (
-                            ""
-                        )}
-                    </>
-                </>
-                :
-                <Grid>
-                    <ProjectName
-                        id={id}
-                        path={path}
-                        project={projectDetails}
-                        userInfo={userInfo}
-                        appliedStudentsList={appliedStudentsList}
-                    />
-                    <ProjectInfo
-                        project={projectDetails} 
-                    />
-                    <Grid.Column width={7}>
-                        <ProjectDates project={projectDetails} />
-                        <ProjectHost project={projectDetails} />
-                    </Grid.Column>
-                    <Grid.Row>
-                        {userInfo.user &&
-                            (userInfo.user.company || userInfo.user.admin) &&
-                            projectDetails.user &&
-                            projectDetails.user.findIndex(chckadmin) !== -1 ? (
-                            <ProjectDetailedAppliedStudents
-                                appliedStudentsList={appliedStudentsList}
-                            />
-                        ) : (
-                            ""
-                        )}
-                    </Grid.Row>
-                </Grid>
-            }
+      {width < 768 ? (
+        <>
+          <ProjectName
+            id={id}
+            path={path}
+            project={projectDetails}
+            userInfo={userInfo}
+            appliedStudentsList={appliedStudentsList}
+          />
+          <ProjectInfo project={projectDetails} />
+          <Grid.Column width={16}>
+            <ProjectDates project={projectDetails} />
+            <ProjectHost project={projectDetails} />
+            <>
+              {userInfo.user &&
+              (userInfo.user.company || userInfo.user.admin) &&
+              projectDetails.user &&
+              projectDetails.user.findIndex(chckadmin) !== -1 ? (
+                <ProjectDetailedAppliedStudents
+                  appliedStudentsList={appliedStudentsList}
+                />
+              ) : (
+                ""
+              )}
+            </>
+          </Grid.Column>
+        </>
+      ) : (
+        <Grid>
+          <ProjectName
+            id={id}
+            path={path}
+            project={projectDetails}
+            userInfo={userInfo}
+            appliedStudentsList={appliedStudentsList}
+          />
+          <ProjectInfo project={projectDetails} />
+          <Grid.Column width={7}>
+            <ProjectDates project={projectDetails} />
+            <ProjectHost project={projectDetails} />
+          </Grid.Column>
+          <Grid.Row>
+            {userInfo.user &&
+            (userInfo.user.company || userInfo.user.admin) &&
+            projectDetails.user &&
+            projectDetails.user.findIndex(chckadmin) !== -1 ? (
+              <ProjectDetailedAppliedStudents
+                appliedStudentsList={appliedStudentsList}
+              />
+            ) : (
+              ""
+            )}
+          </Grid.Row>
         </Grid>
-    );
+      )}
+    </Grid>
+  );
 };
 
 export default ProjectDeatiledPageNeww;
