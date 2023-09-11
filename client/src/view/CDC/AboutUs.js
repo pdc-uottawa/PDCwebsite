2b699c627f7e9b06974b3d8342563725745408ac
synac1 <yanilda.peralta@gmail.com>
2023-09-10 22:12:04 -0400

    task 1 of the cdc request

--- client/src/view/CDC/AboutUs.js
+++ client/src/view/CDC/AboutUs.js
@@ -13,36 +13,18 @@ const ContactUS = (props) => {
     <>
       <div className="container">
         <div>
-          <h1 className="head">ABOUT US</h1>
+          <h1 className="head">Welcome to the Career Development Centre!</h1>
         </div>
         <div className="marginTop">
-          <p className="linkedinDesc">The Career Development Centre is a newly established group under the Professional Development Club of the University of Ottawa, that aims to mentor, guide and help students build their career. Our motto is to inspire students to embrace their potential and strive for career excellence. <br /><br /> We believe that:</p>
-            <div className="row textCenter">
-                <div className="col-md borderItemAbout">
-                    <div>
-                        <h1 className="headAbout">ABILITY</h1>
-                    </div>
-                    <div>
-                        <h5>Is What You Are Capable Of Doing</h5>
-                    </div>
-                </div>
-                <div className="col-md borderItemAbout">
-                    <div>
-                        <h1 className="headAbout">MOTIVATION</h1>
-                    </div>
-                    <div>
-                        <h5>Determines What You Do</h5>
-                    </div>
-                </div>
-                <div className="col-md borderItemAbout">
-                    <div>
-                        <h1 className="headAbout">ATTITUDE</h1>
-                    </div>
-                    <div>
-                        <h5>Determines How Well You Do It</h5>
-                    </div>
-                </div>
-            </div>
+          <p className="linkedinDesc">
+          
+          Welcome to the dynamic hub for personal and professional growth - the Career Development Centre. As an integral part of the Professional Development Club of the University of Ottawa, we are driven to guide you toward your fullest potential and help you find professional success.
+          Whether you are a first-year student exploring prospective career paths, a senior preparing to enter the workforce, or an alum seeking opportunities to advance your career, our team is here to support you every step of the way. Explore our comprehensive range of services, receive personalized guidance from our dedicated team, engage in industry-specific meetups, and immerse yourself in the countless networking opportunities that await you.
+          
+          
+            
+          </p>
+          <p className="linkedinDesc"><strong>Your success story begins right here.</strong></p>
         </div>
       </div>
       <hr />
