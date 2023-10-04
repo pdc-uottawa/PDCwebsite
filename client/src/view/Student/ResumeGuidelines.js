893bf194b4d1a476d7ea45d34ca3bb2c2e5183af
synac1 <yanilda.peralta@gmail.com>
2023-10-04 15:17:42 -0400

    CDC Resume Tips and Guidelines

--- client/src/view/Student/ResumeGuidelines.js
+++ client/src/view/Student/ResumeGuidelines.js
@@ -14,16 +14,127 @@ function ResumeGuidelines(props) {
       </h1>
       <ol class="ui list">
         <li>
-          Make sure you arrange the relevant experience to showcase your
-          candidature.
-        </li>
-        <li>Highlight skills that match the nature of the job</li>
-        <li>Make sure to include LinkedIn profile link</li>
-        <li>Limit adding unnecessary line-spacing and content not relevant</li>
-      </ol>
-      <p>Please get in touch with our Career Development Centre experts for getting your LinkedIn and Resume
-        reviewed and having a 1-1 practical interview call.
-      </p>
+          <h3>Use a Clean and Modern Design: </h3>
+          <p>
+           Employ a clean, professional, and easy-to-read layout.
+          Consider using a modern font and subtle colors to make your resume
+            visually appealing.
+            </p>
+            </li>
+            <li>
+              <h3>Highlight Relevant Skills: </h3>
+              <p>
+                Emphasize your key skills and qualifications prominently at the top
+                of your resume. This helps recruiters quickly identify what you bring
+                  to the table.
+              </p>
+            </li>
+            <li>
+              <h3>Quantify Achievements: </h3>
+              <p>
+                Whenever possible, use quantifiable data to showcase your accomplishments.
+                For example, mention specific percentages, numbers, or results to demonstrate 
+                your impact.
+              </p>
+            </li>
+            <li>
+              <h3>Customize for Each Job Application: </h3>
+              <p>
+                Tailor your resume for each job you apply to by aligning your
+                qualifications and experiences with the specific job requirements.
+                  Use keywords from the job description.
+              </p>
+            </li>
+            <li>
+              <h3>Use a Clean and Modern Design: </h3>
+              <p>
+                Employ a clean, professional, and easy-to-read layout.
+                Consider using a modern font and subtle colors to make your resume
+                visually appealing.
+              </p>
+            </li>
+            <li>
+              <h3>Include a Professional Summary:</h3>
+              <p>
+                Write a concise summary at the beginning of your resume, summarizing your career objectives and what you can bring to a potential employer.
+              </p>
+            </li>
+            <li>
+              <h3>Focus on Achievements, Not Just Duties:</h3>
+              <p>
+                Instead of listing job responsibilities, focus on your achievements and contributions in each role. Show how you added value to your previous employers.
+              </p>
+            </li>
+            <li>
+              <h3>Add a Skills Section:</h3>
+              <p>
+                Include a section that highlights your technical and soft skills relevant to the position you're applying for. This provides a quick overview of your capabilities.
+              </p>
+            </li>
+            <li>
+              <h3>Incorporate Keywords:</h3>
+              <p>
+                Use industry-specific keywords relevant to your field to ensure your resume passes through applicant tracking systems (ATS).
+              </p>
+            </li>
+            <li>
+              <h3>Include Education and Certifications:</h3>
+              <p>
+                List your educational background, including degrees, institutions, and graduation dates. Also, mention any relevant certifications and training.
+              </p>
+            </li>
+            <li>
+              <h3>Use Action Verbs: </h3>
+              <p>
+              Start bullet points with action verbs to make your accomplishments sound more dynamic and engaging.
+              </p>
+            </li>
+            <li>
+              <h3> Include a LinkedIn Profile:</h3>
+              <p>
+                If you have a LinkedIn profile, include the URL on your resume. Make sure your LinkedIn profile is up-to-date and complements your resume.
+              </p>
+            </li>
+            <li>
+              <h3>Keep it Concise: </h3>
+              <p>
+                Aim for a one- or two-page resume. Be concise and only include information directly related to the job you're applying for.
+              </p>
+            </li>
+            <li>
+              <h3>Proofread Carefully: </h3>
+              <p>
+              Eliminate typos and grammatical errors. Consider having someone else review your resume for feedback.
+              </p>
+            </li>
+            <li>
+              <h3>Use an ATS-Friendly Format:</h3>
+              <p>
+              If you're applying online, save your resume in a format that is compatible with applicant tracking systems, such as PDF or Word.
+              </p>
+            </li>
+            <li>
+              <h3>Include Relevant Volunteer Work and Side Projects: </h3>
+              <p>
+              If you have volunteer experience or side projects that demonstrate your skills, include them on your resume, especially if they're relevant to the job.
+              </p>
+            </li>
+            <li>
+              <h3> Update Contact Information: </h3>
+              <p>
+              Ensure your contact information, including phone number and email address, is accurate and up to date.
+              </p>
+            </li>
+            <li>
+              <h3>Consider a Professional Summary Video: </h3>
+              <p>
+                Depending on your industry, a short video summarizing your qualifications and enthusiasm for the role can be a unique addition.
+              </p>
+            </li>
+          </ol>
+          <p>
+          Remember, the key is to make your resume stand out while effectively communicating your qualifications and suitability for the job. Tailor it to each application, and regularly update it as your career progresses.
+          </p>
       <p>
       More information on Career Development services provided by PDC can be found <a href="#/cdc"><u>here</u></a>.
       </p>
